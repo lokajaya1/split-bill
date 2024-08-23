caf4ebebb50646f886ba3ce234a9a6fb659b637e
lokajaya1 <lokajayandala1@gmail.com>
2024-08-23 19:39:00 +0700

    add components Friend, FriendList

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const initialFriends = [
+  {
+    id: 1,
+    name: "John Doe",
+    image: "https://i.pravatar.cc/100?img=1",
+    balance: 50.0,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    image: "https://i.pravatar.cc/100?img=2",
+    balance: -25.0,
+  },
+  {
+    id: 3,
+    name: "Alice Johnson",
+    image: "https://i.pravatar.cc/100?img=3",
+    balance: 0.0,
+  },
+  {
+    id: 4,
+    name: "Bob Brown",
+    image: "https://i.pravatar.cc/100?img=4",
+    balance: 10.5,
+  },
+];
+
+export default initialFriends;
