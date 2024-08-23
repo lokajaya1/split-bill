0b47da643a0b0d6738a523e80d78856175a2c8c2
lokajaya1 <lokajayandala1@gmail.com>
2024-08-23 22:31:19 +0700

    add handle ShowAddFriend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FormAddFriend from "./components/FormAddFriend";
 import FormSplitBill from "./components/FormSplitBill";
 import FriendList from "./components/FriendList";
@@ -7,13 +8,21 @@ import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((setShowAddFriend) => !setShowAddFriend);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <h1>Split Bill</h1>
         <FriendList friends={initialFriends} />
-        <FormAddFriend />
-        <button className="button">Tambah Teman</button>
+        {showAddFriend && <FormAddFriend />}
+        <button className="button" onClick={handleShowAddFriend}>
+          {showAddFriend ? "Tutup" : "Tambah Teman"}
+        </button>
       </div>
       <div>
         <FormSplitBill />
