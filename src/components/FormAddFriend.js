9aa7a193addff53f4cfaace40b9df2b7f3167ede
lokajaya1 <lokajayandala1@gmail.com>
2024-08-25 00:47:10 +0700

    add handleSubmit

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -4,9 +4,27 @@ export default function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/100");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID;
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?={id}`,
+      balance: 0,
+    };
+    onAddFriend(newFriend);
+    setName("");
+    setImage("");
+  }
+
   return (
     <>
-      <form action="" className="form-add-friend">
+      <form action="" className="form-add-friend" onSubmit={handleSubmit}>
         <label htmlFor="name">
           <i className="fa fa-user" aria-hidden="true"></i> Nama
         </label>
