5ea9a095720cf7b62c4bb3d970ecca1f3a7a8f10
lokajaya1 <lokajayandala1@gmail.com>
2024-08-27 00:32:29 +0700

    add logic bill

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FormSplitBill({ selectedFriend }) {
+  const [amount, setAmout] = useState("");
+  const [myBill, setMyBill] = useState("");
+  const friendBill = amount ? amount - myBill : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
   return (
     <>
       <form className="form-split-bill">
@@ -9,22 +14,44 @@ export default function FormSplitBill({ selectedFriend }) {
         <label htmlFor="total-amount">
           <i className="fas fa-money-bill-wave"></i> Total Biaya
         </label>
-        <input id="total-amount" type="number" placeholder="Total biaya" />
+        <input
+          id="total-amount"
+          type="number"
+          placeholder="Total biaya"
+          value={amount}
+          onChange={(e) => setAmout(e.target.value)}
+        />
 
         <label htmlFor="your-bill">
           <i className="fas fa-user"></i> Tagihan Kamu
         </label>
-        <input id="your-bill" type="number" placeholder="Tagihan Kamu" />
+        <input
+          id="your-bill"
+          type="number"
+          placeholder="Tagihan Kamu"
+          value={myBill}
+          onChange={(e) => setMyBill(e.target.value)}
+        />
 
         <label htmlFor="friend-bill">
           <i className="fas fa-users"></i> Tagihan {selectedFriend.name}
         </label>
-        <input id="friend-bill" type="number" placeholder="Tagihan X" />
+        <input
+          id="friend-bill"
+          type="number"
+          placeholder={selectedFriend.name}
+          value={friendBill}
+          disabled
+        />
 
-        <label htmlFor="payee">
+        <label htmlFor="">
           <i className="fas fa-hand-holding-usd"></i> Ditalangin sama
         </label>
-        <select id="payee">
+        <select
+          id=""
+          value={whoIsPaying}
+          onChange={(e) => setWhoIsPaying(e.target.value)}
+        >
           <option value="user">Kamu</option>
           <option value="friend">{selectedFriend.name}</option>
         </select>
